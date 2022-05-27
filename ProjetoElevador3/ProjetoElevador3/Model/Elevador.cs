5fd47b32c0c6454fd89cd3bd2ff2daa82cc92b45
leo89arantes <99375162+leo89arantes@users.noreply.github.com>
2022-05-26 23:25:19 -0300

    Add files via upload

---
+++ ProjetoElevador3/ProjetoElevador3/Model/Elevador.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoElevador3.Model
+{
+    internal class Elevador
+    {
+       
+        //variável responsável pela capacidade total do elevador.
+        public int TotalPessoas { get; set; } //atalho: prop + TAB + TAB
+
+        //variável responsável por informar o andar atual.
+        public int AndarAtual { get; set; }
+
+        //variável responsável por informar quantas pessoas presentes no elevador.
+        public int PessoasNoElevador { get; set; }
+
+        //variável responsável por informar a quantidade de andares.
+        public int TotalAndares { get; set; }
+
+
+        //Método para inicializar o elevador.
+        public void Inicializar(int capacidade, int andares)
+        {
+            TotalPessoas = capacidade;
+            TotalAndares = andares;
+            AndarAtual = 0;
+            PessoasNoElevador = 0;
+
+        }
+
+
+        //Métado para entrar no elevador
+        public bool Entrar()
+        {
+            if (PessoasNoElevador < TotalPessoas)
+            {
+                PessoasNoElevador++;
+                return true;
+            }
+            else
+                return false;
+        }
+
+        //Método para sair do elevador.
+        public bool Sair()
+        {
+            if (PessoasNoElevador > 0)
+            {
+                PessoasNoElevador--;
+                return true;
+            }
+            else
+                return false;
+        }
+
+        //Métado para o elevador subir.
+        public bool Subir()
+        {
+            if (AndarAtual < TotalAndares)
+            {
+                AndarAtual++;
+                return true;
+            }
+            else
+                return false;
+        }
+
+
+        //Método para o elevador descer.
+        public bool Descer()
+        {
+            if (AndarAtual > 0)
+            {
+                AndarAtual--;
+                return true;
+            }
+            else
+                return false;
+        }
+
+
+
+
+        //Pessoasno elevador.
+        public int Pessoas()
+        {
+            return PessoasNoElevador;
+        }
+
+        //Andar em que o elevador está parado.
+        public int Andar()
+        {
+            return AndarAtual;
+        }
+
+    }
+}
---
+++ ProjetoElevador3/ProjetoElevador3/Program.cs
@@ -0,0 +1,130 @@
+﻿using ProjetoElevador3.Model;
+using System;
+
+namespace ProjetoElevador3
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            //O usuário informa a capacidade máxima do elevador.
+            Console.WriteLine("Digite a capcaidade máxima de pessoas que o elevador suporta...: ");
+            var capacidade = int.Parse(Console.ReadLine());
+
+            //O usuário informa a quantidade de andares do prédio.
+            Console.WriteLine("Digite o número máximo de andares do prédio....................: ");
+            var andares = int.Parse(Console.ReadLine());
+
+
+            //traz a capcaidade máxima e o número de andares informados pelo usuário.
+            Elevador elevador = new Elevador();
+            elevador.Inicializar(capacidade, andares);
+
+            //Variáveis para exibir aas mensagens
+            string mensagem = null;
+            string mensagem1 = null;
+
+
+            //Variável para controlar o while
+            int continuar;
+
+
+            do
+            {
+                //Limpa as perguntas inicíais.
+                Console.Clear();
+
+                //Painel de controle.
+                Console.WriteLine($"Capacidade total de pessoas no elevador...: {capacidade}");
+                Console.WriteLine($"Total de andares do prédio................: {andares}");
+                Console.WriteLine($"Pessoas no elevador.......................: {elevador.Pessoas()}");
+                Console.WriteLine($"Andar atual...............................: {elevador.Andar()}\n");
+
+                Console.WriteLine("");
+                Console.WriteLine("");
+
+                Console.WriteLine("::::::::::::::::::::::::::::::::::::::::::::::::::::::");
+                Console.WriteLine(":::::::::::::::::::PAINEL DE OPÇÕES:::::::::::::::::::");
+                Console.WriteLine("::::::::::::::::::::::::::::::::::::::::::::::::::::::");
+
+                Console.WriteLine("");
+
+                //Opções apresentadas oa usuário.
+                Console.WriteLine("1 - Entrar");
+                Console.WriteLine("2 - Sair");
+                Console.WriteLine("3 - Subir");
+                Console.WriteLine("4 - Descer");
+                Console.WriteLine("5 - Fechar o simulador\n");
+
+                Console.WriteLine("::::::::::::::::::::::::::::::::::::::::::::::::::::::\n");
+
+                //Para informar as mensagens que serão exibidas (switch > case > if / else).
+                if (mensagem != null)
+                {
+                    Console.WriteLine($"{mensagem}\n");
+                    mensagem = null;
+                }
+                else
+                    Console.WriteLine($"{mensagem1}\n");
+                mensagem1 = null;
+                
+                Console.Write("Selecione uma opção: ");
+
+                //Verifica se o usuário informou um número inteiro.
+                if (!int.TryParse(Console.ReadLine(), out continuar))
+                    continuar = 0;
+
+                //Início do painel de opção.
+                switch (continuar)
+                {
+                    case 1:
+                        if (!elevador.Entrar())
+                        {
+                            mensagem = "O elevador atingiu a capcaidade total.";
+                        }
+                        else
+                            mensagem1 = "Uma pessoa entrou no elevador.";
+                        break;
+                    case 2:
+                        if (!elevador.Sair())
+                        {
+                            mensagem = "O elevador está vazio.";
+                        }
+                        else
+                            mensagem1 = "Uma pessoa saiu do elevador.";
+                        break;
+                    case 3:
+                        if (!elevador.Subir())
+                        {
+                            mensagem = "Chegamos ao último andar.";
+                        }
+                        else
+                            mensagem1 = "Subindo.";
+                        break;
+                    case 4:
+                        if (!elevador.Descer())
+                        {
+                            mensagem = "Chegamos no Térreo.";
+                        }
+                        else
+                            mensagem1 = "Descendo.";
+                        break;
+                    case 5:
+                        Console.WriteLine("Fechando o simulador");
+                        break;
+
+                    //Mensagem exibida qundo o usuário digitar um número inválido.
+                    default:
+                        mensagem = "Opção inválida!";
+                        break;
+                }
+            }
+            while (continuar != 5);
+        }
+
+
+    }
+    
+}
+
